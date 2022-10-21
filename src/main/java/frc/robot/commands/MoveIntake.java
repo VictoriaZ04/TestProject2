8dff0caa8d7e1311da673e2843f0173b87567b2a
VictoriaZ04 <vzhang0779@gmail.com>
2022-10-20 20:31:26 -0500

    initial commit

---
+++ src/main/java/frc/robot/commands/MoveIntake.java
@@ -0,0 +1,30 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+public class MoveIntake extends CommandBase{
+   
+    public MoveIntake(){
+        addRequirements(RobotContainer.getIntake());
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {
+        RobotContainer.getIntake().move(1);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        RobotContainer.getIntake().stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return !RobotContainer.getJoystick().getRawButton(Constants.INTAKE_BUTTON_ID);
+    }
+}
